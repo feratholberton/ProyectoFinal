82cc3c46ecbc346ecd068332a1cdb1ad404c8ccf
Alejandro Arévalo <126989105+alearecuest@users.noreply.github.com>
2025-10-26 21:55:18 -0300

    app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,6 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 export interface AppOptions extends FastifyServerOptions, Partial<AutoloadPluginOptions> {
 
 }
-// Pass --options via CLI arguments in command to enable these options.
 const options: AppOptions = {
 }
 
@@ -17,22 +16,12 @@ const app: FastifyPluginAsync<AppOptions> = async (
   fastify,
   opts
 ): Promise<void> => {
-  // Place here your custom code!
-
-  // Do not touch the following lines
-
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  // eslint-disable-next-line no-void
+  
   void fastify.register(AutoLoad, {
     dir: join(__dirname, 'plugins'),
     options: opts
   })
 
-  // This loads all plugins defined in routes
-  // define your routes in one of these
-  // eslint-disable-next-line no-void
   void fastify.register(AutoLoad, {
     dir: join(__dirname, 'routes'),
     options: opts
