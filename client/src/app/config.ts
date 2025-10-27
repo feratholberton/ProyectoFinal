d3aa88bb6d7919d9372a5a250fd95b6d85bf6cfa
BrunoBarrera1 <brunoholberton11@gmail.com>
2025-10-27 17:55:09 -0300

    back to latest main commit

--- client/src/app/config.ts
+++
@@ -1,8 +0,0 @@
-const getApiBaseUrl = (): string => {
-  if (typeof window !== 'undefined' && (window as any).APP_API_BASE_URL) {
-    return (window as any).APP_API_BASE_URL;
-  }
-  return 'http://localhost:3000';
-};
-
-export const API_BASE_URL = getApiBaseUrl();
